96d9203c4b2c336da5d7cf5ef3cd9e9a0b73eeff
zerofancy <ntutn.top@gmail.com>
2025-01-18 01:22:07 +0800

    directory chooser

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,24 +2,37 @@ import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import kotlinx.coroutines.launch
 
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        DigestButton()
+    }
+}
+
+@Composable
+fun DigestButton(modifier: Modifier = Modifier) {
+    var selectFileState by remember { mutableStateOf(0) }
+    Button(enabled = selectFileState == 0, onClick = {
+        selectFileState = 1
+    }) {
+        Text("Digest")
+    }
+    val scope = rememberCoroutineScope()
+    LaunchedEffect(selectFileState) {
+        scope.launch {
+            if (selectFileState == 1) {
+                println("select files")
+                val dir = FileChooser.chooseDirectory()
+                println(dir)
+                selectFileState = 0
+            }
         }
     }
 }
