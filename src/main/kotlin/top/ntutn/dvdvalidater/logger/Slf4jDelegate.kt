8e746caf455d270ff4d816895edbb7f7cf6c70e2
zerofancy <ntutn.top@gmail.com>
2025-04-05 21:15:15 +0800

    fix(logger): 优化Slf4jDelegate性能，缓存Logger实例

--- src/main/kotlin/top/ntutn/dvdvalidater/logger/Slf4jDelegate.kt
+++ src/main/kotlin/top/ntutn/dvdvalidater/logger/Slf4jDelegate.kt
@@ -5,9 +5,11 @@ import org.slf4j.LoggerFactory
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
-class Slf4jDelegate(private val tag: String): ReadOnlyProperty<Any?, Logger> {
+class Slf4jDelegate(tag: String): ReadOnlyProperty<Any?, Logger> {
+    private val logger = LoggerFactory.getLogger(tag)
+
     override fun getValue(thisRef: Any?, property: KProperty<*>): Logger {
-        return LoggerFactory.getLogger(tag)
+        return logger
     }
 }
 
