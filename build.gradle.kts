1ae24a13feff7fded5a403b6832993acb28a6c50
zerofancy <ntutn.top@gmail.com>
2025-04-29 02:28:25 +0800

    feat: 更新Gradle配置至8.14并引入版本管理
    scope: gradle
    更新Gradle wrapper至8.14版本
    新增gradle/libs.versions.toml统一管理依赖版本
    移除build.gradle.kts中的硬编码版本号

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,10 @@ import java.net.URI
 import java.net.URL
 
 plugins {
-    kotlin("jvm")
-    id("org.jetbrains.compose")
-    id("org.jetbrains.kotlin.plugin.compose")
-    id("com.google.devtools.ksp") version "2.1.0-1.0.29"
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.ksp)
 }
 
 group = "top.ntutn"
@@ -27,19 +27,12 @@ dependencies {
     // With compose.desktop.common you will also lose @Preview functionality
     implementation(compose.desktop.currentOs)
     implementation(compose.components.resources)
-
-    // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
-    implementation("org.slf4j:slf4j-api:2.0.16")
-    // https://mvnrepository.com/artifact/org.redundent/kotlin-xml-builder
-    implementation("org.redundent:kotlin-xml-builder:1.9.1")
-
-    // autoservice
-    ksp("dev.zacsweers.autoservice:auto-service-ksp:1.2.0")
+    implementation(libs.slf4j.api)
+    implementation(libs.kotlin.xml.builder)
+    ksp(libs.auto.service.ksp)
     // NOTE: It's important that you _don't_ use compileOnly here, as it will fail to resolve at compile-time otherwise
-    implementation("com.google.auto.service:auto-service-annotations:1.1.1")
-
-    // https://mvnrepository.com/artifact/com.vaadin/open
-    implementation("com.vaadin:open:8.5.0.4")
+    implementation(libs.auto.service.annotations)
+    implementation(libs.open)
 }
 
 compose.desktop {
@@ -64,65 +57,7 @@ compose.desktop {
         }
 
         buildTypes.release.proguard {
-            version.set("7.4.0")
             configurationFiles.from(project.file("log.pro"))
-        }
-    }
-}
-
-tasks.create("packageDebWithoutJVM") {
-    dependsOn("packageUberJarForCurrentOS")
-    doLast {
-        println("[${project.name}]Package deb with uber jar...")
-        val checkDpkgProc = Runtime.getRuntime().exec("dpkg --help")
-        checkDpkgProc.waitFor()
-        if (checkDpkgProc.exitValue() != 0) {
-            println("dpkg not found!")
-            checkDpkgProc.errorStream.readAllBytes().decodeToString().let(::println)
-            return@doLast
-        }
-        val jarDir = layout.buildDirectory.file("compose/jars").get().asFile
-        val jarFile = jarDir.listFiles { file: File ->
-            file.extension == "jar" && file.name.startsWith(project.name)
-        }?.first()
-        if (jarFile == null) {
-            println("output jar not found")
-            return@doLast
-        }
-        println("found jar file $jarFile")
-        val outputDir = layout.buildDirectory.file("compose/jar2deb/${project.name}").get().asFile
-        println("output dir $outputDir")
-        if (outputDir.exists()) {
-            outputDir.deleteRecursively()
-        }
-        outputDir.mkdirs()
-        val debianDir = File(outputDir, "DEBIAN").also(File::mkdir)
-        val installDir = File(outputDir, "opt/${project.name}").also(File::mkdir)
-        val destJarFile = File(installDir, "${project.name}.jar")
-        jarFile.copyTo(destJarFile)
-        // todo generate shell script to launch it
-        val debianControlFile = File(debianDir, "control")
-        """
-            Package: ${project.name}
-            Version: 1.0
-            Section: custom
-            Priority: optional
-            Architecture: all
-            Depends: openjdk-17-jre | java17-runtime | java17-runtime-headless | openjdk-17-jre-headless
-            Maintainer: zerofancy <ntutn.top@gmail.com>
-            Description: This is a compose app package without jvm
-            
-        """.trimIndent().let {
-            debianControlFile.writeText(it)
-        }
-        println("all files ready, start building deb: $outputDir")
-        val debProc = Runtime.getRuntime().exec("fakeroot dpkg-deb --build ${outputDir.name}", arrayOf(), outputDir.parentFile)
-        debProc.waitFor()
-        debProc.inputStream.readAllBytes().decodeToString().let(::println)
-        if (debProc.exitValue() != 0) {
-            debProc.errorStream.readAllBytes().decodeToString().let(::println)
-        } else {
-            println("$outputDir.jar built success")
         }
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,12 +5,6 @@ pluginManagement {
         gradlePluginPortal()
         mavenCentral()
     }
-
-    plugins {
-        kotlin("jvm").version(extra["kotlin.version"] as String)
-        id("org.jetbrains.compose").version(extra["compose.version"] as String)
-        id("org.jetbrains.kotlin.plugin.compose").version(extra["kotlin.version"] as String)
-    }
 }
 
 rootProject.name = "DVDValidater"
